30738d68182c995ce8e6bb3333f6a54bed865b8b
ldx <light_black@126.com>
2019-12-19 21:42:27 +0800

    学习使用dom4j xpath 工具 第一天

--- src/Dom4JforGooglEearth_Kml/Dom4JforKml.java
+++ src/Dom4JforGooglEearth_Kml/Dom4JforKml.java
@@ -1,67 +1,55 @@
 package Dom4JforGooglEearth_Kml;
 
-import Dom4j.Entity.Book;
-import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 
 public class Dom4JforKml {
+
     public static void main(String[] args) throws MalformedURLException {
-        ArrayList<Book> bookList = new ArrayList<Book>();
-        //1.创建SAXReader的对象reader
-        SAXReader reader = new SAXReader();
+        //ArrayList<Book> bookList = new ArrayList<Book>();
+        URL url = new URL("file:D:/programming/IDEA_Dev/readkml4method/沪昆线12.11.kml");
         try {
             //2.通过reader对象的read（）方法加载books.xml文件，获取document对象
-            Document document = reader.read(new File("沪昆线12.11.kml"));
-            //3.通过document对象获取根节点bookstore
-            Element documentRootElement = document.getRootElement();
-            System.out.println("RootElement : " + documentRootElement.getName());
-            //4.通过element对象的elementIterator获取迭代器
-            Iterator it = documentRootElement.elementIterator();
-            //5.遍历迭代器，获取根节点中的信息
-            while (it.hasNext()){
-                System.out.println("==========开始遍历某一本书==========");
-                Element book = (Element)it.next();
-                if(book.getName() == "Folder") {
+            Document document = parse(url);
+            //3.通过document对象获取根节点
+            Element root = document.getRootElement();
+            //System.out.println(root.getName());
 
+            List<Element> nodeList = root.elements();
+            for (Element e:nodeList) {
+
+                List<Element> nodeList2 = e.elements();
+                for(Element e2:nodeList2) {
+                    System.out.println(e2.getName());
                 }
-
-                Book bookData = new Book();
-
-                //6.获取book的属性名和属性值
-                List<Attribute> bookAttrs = book.attributes();
-                for (Attribute attr : bookAttrs) {
-                    System.out.println("属性名：" + attr.getName() + "--" + "属性值：" + attr.getValue());
-
-                    if (attr.getName().equals("id")) {
-                        bookData.setId(attr.getValue());
-                    }
-                }
-                //7.通过book对象的elementIterator获取节点元素迭代器
-                Iterator itt = book.elementIterator();
-
-                //8.遍历迭代器，获取子节点中的信息
-                while (itt.hasNext()) {
-                    Element bookChild = (Element)itt.next();
-
-                    //9.获取节点名和节点值
-                    System.out.println("节点名：" + bookChild.getName() + "---节点值：" + bookChild.getStringValue());
-
-                }
-
+                System.out.println(e.getName());
             }
-        } catch (DocumentException e) {
-            e.printStackTrace();
+
+            } catch (DocumentException e1) {
+            e1.printStackTrace();
         }
+    }
 
 
+    //1.创建SAXReader的对象reader
+    public static Document parse(URL url) throws DocumentException {
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(url);
+        return document;
+    }
+    // 遍历xml节点
+    public void selectnode(Document document) throws DocumentException {
+        Element root = document.getRootElement();
+        // iterate through child elements of root
+        for (Iterator<Element> it = root.elementIterator(); it.hasNext();) {
+            Element element = it.next();
+        }
     }
 }
--- src/Dom4JforGooglEearth_Kml/Entity/Node.java
+++ src/Dom4JforGooglEearth_Kml/Entity/Node.java
@@ -2,4 +2,5 @@ package Dom4JforGooglEearth_Kml.Entity;
 
 public class Node {
     private double longtitude;
+    private double altitude;
 }
